0aa03f80435757fe40e03988af2fd6d0b631ce8a
mercynjambi <njambim695@gmail.com>
2024-08-24 12:18:32 +0300

    passed the tests

--- server/app.py
+++ server/app.py
@@ -46,6 +46,31 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+        
+        db.session.commit()
+
+        
+        
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        
+        db.session.delete(plant)
+        db.session.commit()
+        
+        return make_response('', 204)
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
